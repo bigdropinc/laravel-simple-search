a8c140c6e73e5f1a36db3c85ef796ec1c6fd2a31
roman.o <roman.o@bigdropinc.com>
2019-09-25 11:08:44 +0300

    fix setAttributes

--- src/SearchBuilder.php
+++ src/SearchBuilder.php
@@ -122,7 +122,7 @@ abstract class SearchBuilder
 
         $temp = [];
         foreach ($attributes as $key => $value) {
-            if (!\in_array($key, $this->fillable)) {
+            if (!\in_array($key, $this->fillable) || empty($value)) {
                 continue;
             }
 
